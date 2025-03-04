d9f666b7de0c97438c5f26910c956b92b18e00c0
cgamemfgc <cgamemfgc@gmail.com>
2025-03-04 22:38:27 +0900

    Initial Commit

---
+++ iOS Calculator App/iOS_Calculator_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Calculator_AppApp.swift
+//  iOS Calculator App
+//
+//  Created by Takeshi Sakamoto on 2025/03/04.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Calculator_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS Calculator AppTests/iOS_Calculator_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Calculator_AppTests.swift
+//  iOS Calculator AppTests
+//
+//  Created by Takeshi Sakamoto on 2025/03/04.
+//
+
+import Testing
+@testable import iOS_Calculator_App
+
+struct iOS_Calculator_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
